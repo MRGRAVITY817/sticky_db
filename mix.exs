88201007b1c58876097e9557b8629caf1ca509ec
Hoon Wee <mrgravity817@gmail.com>
2024-01-04 13:12:57 +0900

    feat: add changelog md

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule StickyDb.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+  @repo_url "https://github.com/MRGRAVITY817/sticky_db"
+
   def project do
     [
       app: :sticky_db,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      description: "Ecto adapters for GlueSQL Elixir library - It's very sticky!",
+      package: package()
     ]
   end
 
@@ -22,6 +28,32 @@ defmodule StickyDb.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "StickyDB",
+      extras: ["CHANGELOG.md"],
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"],
+      source_ref: "v#{@version}",
+      source_url: @repo_url
+    ]
+  end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "native",
+        "mix.exs",
+        "README.md",
+        "CHANGELOG.md",
+        "LICENSE"
+      ],
+      maintainers: ["Hoon Wee"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @repo_url}
     ]
   end
 end
